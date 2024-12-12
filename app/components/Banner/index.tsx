20cbdb2fd6cb0a0ed719e6512500f8cd226651f9
ChedlyJenhani <chedlidon@gmail.com>
2024-12-12 20:58:14 +0100

    first commit

---
+++ app/components/Banner/index.tsx
@@ -0,0 +1,42 @@
+"use client"
+import Image from 'next/image';
+import { Fade } from "react-awesome-reveal";
+import Link from 'next/link';
+
+
+const Banner = () => {
+
+    return (
+        <div id="home-section" className='bg-lightpink'>
+            <div className="mx-auto max-w-7xl pt-20 sm:pb-24 px-6">
+
+                <div className='grid grid-cols-1 lg:grid-cols-12 space-x-1'>
+
+                    <div className='col-span-6 flex flex-col justify-center'>
+                        <Fade direction={'up'} delay={400} cascade damping={1e-1} triggerOnce={true}>
+                            <h1 className="text-4xl lg:text-7xl font-semibold mb-5 text-lightgrey md:4px lg:text-start text-center">
+                            Bienvenue sur le site <span>A N O U H</span> 
+                            </h1>
+                        </Fade>
+                        <Fade direction={'up'} delay={800} cascade damping={1e-1} triggerOnce={true}>
+                            <p className='text-grey lg:text-lg font-normal mb-10 lg:text-start text-center'>Découvrez nos produits frais et de qualité supérieure, parfaits pour vos besoins quotidiens.</p>
+                        </Fade>
+                        <Fade direction={'up'} delay={1000} cascade damping={1e-1} triggerOnce={true}>
+                            <div className='md:flex align-middle justify-center lg:justify-start'>
+                                <button className='text-xl w-full md:w-auto font-medium rounded-full text-white py-5 px-6 bg-pink lg:px-14 mr-6'><Link href='#cook-section'>Lets cook</Link></button>
+                                <button className='flex border w-full md:w-auto mt-5 md:mt-0 border-pink justify-center rounded-full text-xl font-medium items-center py-5 px-10 text-pink hover:text-white hover:bg-pink'><Link href='#about-section'>Explore now</Link></button>
+                            </div>
+                        </Fade>
+                    </div>
+
+                    <div className='col-span-6 flex justify-center relative'>
+                        
+                        <Image src="/images/Banner/baner.png" alt="nothing" width={1000} height={805} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
---
+++ app/components/Cook/index.tsx
@@ -0,0 +1,52 @@
+"use client"
+import Image from 'next/image';
+import { Fade } from "react-awesome-reveal";
+
+
+const Cook = () => {
+
+    return (
+        <div className='relative' id="cook-section">
+            <div className="mx-auto max-w-7xl lg:pt-20 sm:pb-24 px-6">
+
+                <div className='absolute right-0 bottom-[-18%] hidden lg:block'>
+                    <Image src={'/images/Cook/fanta.png'} alt="burger-image" width={463} height={622} />
+                </div>
+
+                <div className='grid grid-cols-1 lg:grid-cols-12 my-16 space-x-5'>
+
+                    <div className='col-span-6 flex justify-start'>
+                        <Image src="/images/Cook/tel.png" alt="nothing" width={636} height={808} />
+                    </div>
+
+
+                    <div className='col-span-6 flex flex-col justify-center'>
+                        <Fade direction={'up'} delay={400} cascade damping={1e-1} triggerOnce={true}>
+                            <h2 className='text-pink text-lg font-normal mb-3 ls-51 uppercase text-start'>Livraison Rapide</h2>
+                        </Fade>
+                        <Fade direction={'up'} delay={800} cascade damping={1e-1} triggerOnce={true}>
+                            <h3 className="text-3xl lg:text-5xl font-semibold text-black text-start">
+                            Livré rapidement par des experts.
+                            </h3>
+                        </Fade>
+                        <Fade direction={'up'} delay={1000} cascade damping={1e-1} triggerOnce={true}>
+                            <p className='text-grey md:text-lg font-normal mb-10 text-start mt-2'>Profitez de notre service de livraison rapide, garantissant que vos commandes seront entre vos mains en moins de 24 heures. Nous comprenons l'importance de la rapidité dans le monde des affaires et nous nous engageons à vous offrir la meilleure expérience de livraison. </p>
+                            <p className='text-grey md:text-lg font-normal mb-10 text-start mt-1'>Naviguez facilement à travers notre interface conviviale et intuitive.Que vous soyez un nouveau commerçant ou un habitué de la vente en ligne, notre plateforme est conçue pour être facile à utiliser, vous permettant ainsi de gérer vos commandes et votre inventaire en toute simplicité.
+
+....</p>
+                            <div className='flex align-middle justify-center md:justify-start'>
+                                <button className='text-xl font-medium rounded-full text-white py-5 px-6 bg-pink lg:px-10 mr-6'>Learn more</button>
+                            </div>
+                        </Fade>
+                    </div>
+
+
+
+                </div>
+
+            </div>
+        </div >
+    )
+}
+
+export default Cook;
