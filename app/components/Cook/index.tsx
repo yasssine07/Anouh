5e0a6467cb0fe3617708e2be69893d6e269a61b4
ChedlyJenhani <chedlidon@gmail.com>
2024-12-13 19:08:54 +0100

    Fix unescaped character in Cook component

--- app/components/Cook/index.tsx
+++ app/components/Cook/index.tsx
@@ -22,16 +22,18 @@ const Cook = () => {
 
                     <div className='col-span-6 flex flex-col justify-center'>
                         <Fade direction={'up'} delay={400} cascade damping={1e-1} triggerOnce={true}>
-                            <h2 className='text-pink text-lg font-normal mb-3 ls-51 uppercase text-start'>{'Livraison Rapide'}</h2>
+                            <h2 className='text-pink text-lg font-normal mb-3 ls-51 uppercase text-start'>Livraison Rapide</h2>
                         </Fade>
                         <Fade direction={'up'} delay={800} cascade damping={1e-1} triggerOnce={true}>
                             <h3 className="text-3xl lg:text-5xl font-semibold text-black text-start">
-                            {'Livré rapidement par des experts.'}
+                            Livré rapidement par des experts.
                             </h3>
                         </Fade>
                         <Fade direction={'up'} delay={1000} cascade damping={1e-1} triggerOnce={true}>
-                            <p className='text-grey md:text-lg font-normal mb-10 text-start mt-2'>{'Profitez de notre service de livraison rapide, garantissant que vos commandes seront entre vos mains en moins de 24 heures. Nous comprenons l importance de la rapidité dans le monde des affaires et nous nous engageons à vous offrir la meilleure expérience de livraison.'} </p>
-                            <p className='text-grey md:text-lg font-normal mb-10 text-start mt-1'>{'Naviguez facilement à travers notre interface conviviale et intuitive.Que vous soyez un nouveau commerçant ou un habitué de la vente en ligne, notre plateforme est conçue pour être facile à utiliser, vous permettant ainsi de gérer vos commandes et votre inventaire en toute simplicité....'}</p>
+                        <p className='text-grey md:text-lg font-normal mb-10 text-start mt-1'>Nous comprenons l&apos;importance de la rapidité dans le monde des affaires et nous nous engageons à vous offrir la meilleure expérience de livraison.</p>
+                        <p className='text-grey md:text-lg font-normal mb-10 text-start mt-1'>Naviguez facilement à travers notre interface conviviale et intuitive.Que vous soyez un nouveau commerçant ou un habitué de la vente en ligne, notre plateforme est conçue pour être facile à utiliser, vous permettant ainsi de gérer vos commandes et votre inventaire en toute simplicité.
+
+....</p>
                             <div className='flex align-middle justify-center md:justify-start'>
                                 <button className='text-xl font-medium rounded-full text-white py-5 px-6 bg-pink lg:px-10 mr-6'>Learn more</button>
                             </div>
